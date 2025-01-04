66597011d42b0a947fe3f677c6bdf8ee59f75f5c
leenap <138697600+leenap2708@users.noreply.github.com>
2025-01-04 10:03:50 -0800

    Update Module3SilulationLab2.c

--- Module3SilulationLab2.c
+++ Module3SilulationLab2.c
@@ -1,17 +1,24 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
- 
-#define BUFSIZE 256
-    
-// This program prints the size of a specified file in bytes
+
 int main(int argc, char** argv) {
-    // Ensure that the user supplied exactly one command line argument
-    if (argc != 2) { 
+    if (argc != 2) {
         fprintf(stderr, "Please provide the address of a file as an input.\n");
-        return -1;
+        return EXIT_FAILURE;
     }
-    char cmd[BUFSIZE] = "wc -c < ";
-    strcat(cmd, argv[1]);
-    system(cmd);
+
+    FILE* file = fopen(argv[1], "r");
+    if (file == NULL) {
+        perror("Failed to open file");
+        return EXIT_FAILURE;
+    }
+
+    fseek(file, 0, SEEK_END);
+    long file_size = ftell(file);
+    fclose(file);
+
+    printf("File size: %ld bytes\n", file_size);
+
+    return EXIT_SUCCESS;
 }
